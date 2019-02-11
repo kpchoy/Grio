287a15e4f076ed322129b77452c6717cf3adbc60
Kevin Choy <kevinchoy22@gmail.com>
2019-02-10 17:22:20 -0800

    modal functions properly and increments counter

--- frontend/components/counter/counter.jsx
+++ frontend/components/counter/counter.jsx
@@ -1,12 +1,28 @@
 import React, { Component } from 'react';
+import Modal from 'react-modal';
+
+const customStyles = {
+  content : {
+    top                   : '50%',
+    left                  : '50%',
+    right                 : 'auto',
+    bottom                : 'auto',
+    marginRight           : '-50%',
+    transform             : 'translate(-50%, -50%)'
+  }
+};
 
 export default class Counter extends Component {
   constructor(props) {
     super(props);
     this.state = {
       count: 0,
+      nextCount:0,
+      modalIsOpen: false,
     };
     this.increment = this.increment.bind(this);
+    this.openModal = this.openModal.bind(this);
+    this.closeModal = this.closeModal.bind(this);
   }
 
   increment() {
@@ -16,14 +32,51 @@ export default class Counter extends Component {
     } else {
       start++;
     }
-    this.setState({count: start});
+    this.setState({
+      count: start,
+      modalIsOpen: false,
+    });
   }
+
+  openModal() {
+    let start = this.state.count;
+    if (start > 1) {
+      start = start * 2;
+    } else {
+      start++;
+    }
+    this.setState({
+      modalIsOpen: true,
+      nextCount: start,
+    });
+
+  }
+
+  closeModal() {
+    this.setState({modalIsOpen: false});
+  }
+
 
   render() {
     return (
       <div className="counter-main" >
         <h2>Count: {this.state.count} </h2>
-        <button onClick={this.increment}>Increment</button>
+        <button onClick={this.openModal}>Increment</button>
+        <Modal
+          isOpen={this.state.modalIsOpen}
+          onRequestClose={this.closeModal}
+          style={customStyles}
+          contentLabel="Example Modal"
+        >
+          <form>
+            <h3>Current Count : {this.state.count}</h3>
+            <h3>Next Count: {this.state.nextCount}</h3>
+
+            <button onClick={this.increment}>Confirm</button>
+            <button onClick={this.closeModal}>Cancel</button>
+          </form>
+        </Modal>
+       
       </div>
     );
   }
